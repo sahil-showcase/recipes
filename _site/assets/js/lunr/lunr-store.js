f403c09ba0433c0a0c19d43f1659c7084ad169b9
sahil <91165890+sahil-showcase@users.noreply.github.com>
2025-01-08 18:57:09 -0500

    retry

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Spaghetti Bolognese",
-        "excerpt":"   In a large pan, sauté onion and garlic…   Add ground beef…  ","categories": [],
+        "excerpt":"   In a large pan, sauté onion and garlic…   Add ground beef and…  ","categories": [],
         "tags": [],
         "url": "/recipes/confit-duck-lorange/",
         "teaser": null
